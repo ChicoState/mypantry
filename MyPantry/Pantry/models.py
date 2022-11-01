dcfe1d57039b597b9179f2f09e812a7f2a53fa88
rqheldt <rqheldt@mail.csuchico.edu>
2022-11-01 02:35:18 -0700

    fixing model stuff so i can put the recipes into the database

--- MyPantry/Pantry/models.py
+++ MyPantry/Pantry/models.py
@@ -5,21 +5,22 @@ from django.contrib.auth.models import User
 
 
 # Create your models here.
+class recipe(models.Model):
+    title = models.CharField(max_length=255)
+    directions = models.TextField()
+    content = models.TextField()
+    category = models.CharField(max_length=255)
+
+    def __str__(self):
+        return self.title
+
 class ingredient(models.Model):
     name =  models.CharField(max_length=255)
     quantity = models.CharField(max_length=20)
     unit =  models.CharField(max_length=50)
     user = models.ForeignKey(User, blank=True, null=True, on_delete=models.CASCADE)
+    recipe = models.ForeignKey(recipe, blank=True, null=True, on_delete=models.CASCADE)
     
-class recipe(models.Model):
-    title = models.CharField(max_length=255)
-    directions = models.TextField()
-    content = models.TextField()
-    ingredients = models.ForeignKey(ingredient,on_delete=models.CASCADE, null=True)
-    category = models.CharField(max_length=255)
-
-    def __str__(self):
-        return self.title
 
 class UserModel(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
